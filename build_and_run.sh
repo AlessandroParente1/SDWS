083684923116ec90b610ca6e6785cc5229eb1ca6
s250120 <s250120@dtu.dk>
2026-01-09 12:05:48 +0100

    Deployment files

---
+++ build_and_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+pushd day2/simple_REST_API
+mvn clean package
+# Create a new docker image if necessary.
+docker compose build
+# Restarts the container with the new image if necessary
+docker compose up -d
+# The server stays running.
+# To terminate the server run docker-compose down in the
+# simple-rest directory
+# clean up images
+docker image prune -f 
+popd
+
+# Give the Web server a chance to finish start up
+sleep 2 
+
+pushd client 
+mvn clean test
+popd
